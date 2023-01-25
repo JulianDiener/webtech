f9216a782ebd44bca590c732eede1248a4deb844
JulianDiener <julian.diener@stud.hawk.de>
2023-01-25 21:49:20 +0100

    CHG: added links
    It's possible to navigate through pages with links

--- src/webpages/aboutus.js
+++ src/webpages/aboutus.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 const Aboutus = () => {
     return (
         <div>
             <h1>About us</h1>
             <p>This is about us page</p>
+            <li><Link to="/aboutus">About us</Link></li>
         </div>
     );
 };
--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {
+    Link
+} from "react-router-dom";
 const Home = () => {
     return (
         <div>
             <h1>Welcome to Xcelvations</h1>
             <p>This is home page</p>
+            <li><Link to="/aboutus">About us</Link></li>
         </div>
     );
 };
