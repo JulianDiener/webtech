a2a311faa47a1932963e239e5256c4e13ef0d19b
JulianDiener <julian.diener@stud.hawk.de>
2023-01-25 21:17:45 +0100

    ADD: added index Page

---
+++ src/webpages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from "react-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from './home';
+import Aboutus from './aboutus';
+const Webpages = () => {
+    return(
+        <Router>
+            <Routes>
+            <Route exact path="/" element = {<Home />} />
+            <Route path = "/aboutus" element = {<Aboutus />} />
+            </Routes>
+        </Router>
+    );
+};
+export default Webpages;
