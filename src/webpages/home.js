272dfbbfc45886cbc58c090364d0e88a74fa03cb
JulianDiener <julian.diener@stud.hawk.de>
2023-02-10 18:46:35 +0100

    CHG: added function to represent picture
    picture will be displayed with name and type

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -4,6 +4,15 @@ import {
     Link
 } from "react-router-dom";
 
+function Im(props) {
+       return (<div>
+               <img src={data.pictures[props.number].src} alt="Test"/>
+               <p>{data.pictures[props.number].name}</p>
+               <p>{data.pictures[props.number].type}</p>
+       </div>
+                );
+}
+
 const Home = () => {
     return (
         <div>
