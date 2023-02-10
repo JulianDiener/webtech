0961b96f753fe9815e52f64be61e65d9ec5327a4
JulianDiener <julian.diener@stud.hawk.de>
2023-02-10 17:37:00 +0100

    CHG: used pictures.json

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import data from './pictures.json';
 import {
     Link
 } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to Xcelvations</h1>
-            <p>This is home page</p>
+            <img src={data.pictures[0].src} alt="Test"/>
+            <p>{data.pictures[0].name}</p>
+            <p>{data.pictures[0].type}</p>
             <li><Link to="/aboutus">About us</Link></li>
         </div>
     );
