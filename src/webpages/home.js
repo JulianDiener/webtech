6423cb83fa0a2bf5a74f8110efa86f78f7060ff2
JulianDiener <julian.diener@stud.hawk.de>
2023-02-10 20:31:27 +0100

    CHG: added List of pictures
    pictures will now align next to each other in a scroll slide. You can add or remove pictures with changing the numbers list

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -4,20 +4,31 @@ import {
     Link
 } from "react-router-dom";
 
-function Im(props) {
-       return (<div>
-               <img src={data.pictures[props.number].src} alt="Test"/>
-               <p>{data.pictures[props.number].name}</p>
-               <p>{data.pictures[props.number].type}</p>
-       </div>
-                );
+function PictureItem(props_1) {
+    return <div style={{ display: "inline-block"}}>
+        <img src={data.pictures[props_1.value].src} alt="Test"/>
+        <p>{data.pictures[props_1.value].name}</p>
+        <p>{data.pictures[props_1.value].type}</p></div>;
 }
+
+function PictureList(props_2) {
+    const numbers = props_2.numbers;
+    const pictureItems = numbers.map((number) =>
+        <PictureItem value={number} />
+    );
+    return (
+        <div style={{ overflow: "scroll", whiteSpace: "nowrap" }}>
+            {pictureItems}
+        </div>
+    );
+}
+const numbers = [0, 1, 0, 1, 0, 1, 0, 1];
+
 
 const Home = () => {
     return (
         <div>
-            <Im number={0} />
-            <Im number={1} />
+            <PictureList numbers={numbers} />
             <li><Link to="/aboutus">About us</Link></li>
         </div>
     );
