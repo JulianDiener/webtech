540cb7a6e2af85604cf814817d160e266202e02a
JulianDiener <julian.diener@stud.hawk.de>
2023-02-11 17:45:00 +0100

    CHG: added mouseOver and mouseOut events
    used hook to handle state of mouse. When mouse is over-> Backgroundcolr="black" when mouse is out -> Backgroundcolr="white"

--- src/components/pictureItem.js
+++ src/components/pictureItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 
 function PictureItem(props) {
+    const [isMouseOver, setMouseOver] = useState(false);
+    function handleMouseOver(){
+        setMouseOver(true);
+    }
+    function handleMouseOut(){
+        setMouseOver(false);
+    }
     return <div style={{ display: "inline-block"}}>
-        <button>
+        <button
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            style={{backgroundColor: isMouseOver? "grey" : "white"}}>
         <img src={props.src} alt="Test"/>
         <p>{props.name}</p>
         <p>{props.type}</p>
