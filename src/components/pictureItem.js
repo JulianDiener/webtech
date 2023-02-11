d6c95fc69d9f920a9925926ab063f6da8c8b9784
JulianDiener <julian.diener@stud.hawk.de>
2023-02-11 17:33:39 +0100

    CHG: turned pictureItems into a button

--- src/components/pictureItem.js
+++ src/components/pictureItem.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 function PictureItem(props) {
     return <div style={{ display: "inline-block"}}>
+        <button>
         <img src={props.src} alt="Test"/>
         <p>{props.name}</p>
-        <p>{props.type}</p></div>;
+        <p>{props.type}</p>
+        </button></div>;
 }
 
 export default PictureItem;
