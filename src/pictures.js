148683471fd64265f159c14529f641cca25ecc85
JulianDiener <julian.diener@stud.hawk.de>
2023-02-11 11:53:22 +0100

    CHG: changed pictures.json to a javascript element

---
+++ src/pictures.js
@@ -0,0 +1,62 @@
+const pictures =
+          [
+            {
+              "id" : 1,
+              "name": "testbild",
+              "src": "/images/Unbenannt-1.jpg",
+              "type": "Landscape",
+              "destination": "Africa"
+            },
+            {
+              "id" : 2,
+              "name": "Yukee",
+              "src": "/images/Unbenannt-2.jpg",
+              "type": "Portrait",
+              "destination": "Berlin"
+            },
+              {
+                  "id" : 3,
+                  "name": "testbild",
+                  "src": "/images/Unbenannt-1.jpg",
+                  "type": "Landscape",
+                  "destination": "Africa"
+              },
+              {
+                  "id" : 4,
+                  "name": "Yukee",
+                  "src": "/images/Unbenannt-2.jpg",
+                  "type": "Portrait",
+                  "destination": "Berlin"
+              },
+              {
+                  "id" : 5,
+                  "name": "testbild",
+                  "src": "/images/Unbenannt-1.jpg",
+                  "type": "Landscape",
+                  "destination": "Africa"
+              },
+              {
+                  "id" : 6,
+                  "name": "Yukee",
+                  "src": "/images/Unbenannt-2.jpg",
+                  "type": "Portrait",
+                  "destination": "Berlin"
+              },
+              {
+                  "id" : 7,
+                  "name": "testbild",
+                  "src": "/images/Unbenannt-1.jpg",
+                  "type": "Landscape",
+                  "destination": "Africa"
+              },
+              {
+                  "id" : 8,
+                  "name": "Yukee",
+                  "src": "/images/Unbenannt-2.jpg",
+                  "type": "Portrait",
+                  "destination": "Berlin"
+              }
+          ]
+
+
+export default pictures;
