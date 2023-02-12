6e0f3f45f6ce90e99432e25e5980d7bdd4b739dc
JulianDiener <julian.diener@stud.hawk.de>
2023-02-12 22:33:25 +0100

    ADD: added italian pictures

--- src/pictures.js
+++ src/pictures.js
@@ -1,62 +1,78 @@
-const pictures =
+const picturesItalia =
           [
             {
               "id" : 1,
-              "name": "testbild",
-              "src": "/images/Unbenannt-1.jpg",
-              "type": "Landscape",
-              "destination": "Africa"
-            },
-            {
-              "id" : 2,
-              "name": "Yukee",
-              "src": "/images/Unbenannt-2.jpg",
-              "type": "Portrait",
-              "destination": "Berlin"
+              "name": "San Vito Lo Capo Rustico",
+              "src": "/images/Italia/SanVitoLoCapoRustico.jpg",
+              "price": "110 €",
+              "destination": "Italien"
             },
               {
+                  "id" : 2,
+                  "name": "CASETTA LAVANDA",
+                  "src": "/images/Italia/CASETTALAVANDA.jpg",
+                  "price": "141 €",
+                  "destination": "Italien"
+              },
+              {
                   "id" : 3,
-                  "name": "testbild",
-                  "src": "/images/Unbenannt-1.jpg",
-                  "type": "Landscape",
-                  "destination": "Africa"
+                  "name": "Agriturismo with pool",
+                  "src": "/images/Italia/Agriturismowithpool.jpg",
+                  "price": "210 €",
+                  "destination": "Italien"
               },
               {
                   "id" : 4,
-                  "name": "Yukee",
-                  "src": "/images/Unbenannt-2.jpg",
-                  "type": "Portrait",
-                  "destination": "Berlin"
+                  "name": "Beautiful barn",
+                  "src": "/images/Italia/Beautifulbarn.jpg",
+                  "price": "199 €",
+                  "destination": "Italien"
               },
               {
                   "id" : 5,
-                  "name": "testbild",
-                  "src": "/images/Unbenannt-1.jpg",
-                  "type": "Landscape",
-                  "destination": "Africa"
+                  "name": "Trullo with Swimming Pool",
+                  "src": "/images/Italia/Trullo with Swimming Pool.jpg",
+                  "price": "120 €",
+                  "destination": "Italien"
               },
               {
                   "id" : 6,
-                  "name": "Yukee",
-                  "src": "/images/Unbenannt-2.jpg",
-                  "type": "Portrait",
-                  "destination": "Berlin"
+                  "name": "Tuscan apartment",
+                  "src": "/images/Italia/Tuscan apartment.jpg",
+                  "price": "150 €",
+                  "destination": "Italien"
               },
               {
                   "id" : 7,
-                  "name": "testbild",
-                  "src": "/images/Unbenannt-1.jpg",
-                  "type": "Landscape",
-                  "destination": "Africa"
+                  "name": "Villa Privata",
+                  "src": "/images/Italia/Villa Privata.jpg",
+                  "price": "221 €",
+                  "destination": "Italien"
               },
               {
                   "id" : 8,
-                  "name": "Yukee",
-                  "src": "/images/Unbenannt-2.jpg",
-                  "type": "Portrait",
-                  "destination": "Berlin"
+                  "name": "Villa Anna",
+                  "src": "/images/Italia/Villa Anna.jpg",
+                  "price": "180 €",
+                  "destination": "Italien"
+              },
+              {
+                  "id" : 9,
+                  "name": "Cascina Burroni",
+                  "src": "/images/Italia/Cascina Burroni.jpg",
+                  "price": "136 €",
+                  "destination": "Italien"
+              },
+              {
+                  "id" : 10,
+                  "name": "SPECTACULAR SEA VIEW",
+                  "src": "/images/Italia/SPECTACULAR SEA VIEW.jpg",
+                  "price": "207 €",
+                  "destination": "Italien"
               }
+
           ]
 
 
-export default pictures;
+export default picturesItalia;
+export {picturesItalia};
