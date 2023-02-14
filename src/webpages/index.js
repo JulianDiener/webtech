9e169576cc7d61fbda7587551adc7cfa2e522dc5
Julian Diener <juliandiener2@gmail.com>
2023-02-14 12:25:22 +0100

    CHG: removed jQuery from project

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -6,14 +6,12 @@ import {
 } from "react-router-dom";
 import Home from './home';
 import Aboutus from './aboutus';
-import Jquery from "./jquery";
 const Webpages = () => {
     return(
         <Router>
             <Routes>
             <Route exact path="/" element = {<Home />} />
             <Route path = "/aboutus" element = {<Aboutus />} />
-                <Route path = "/jquery" element = {<Jquery />} />
             </Routes>
         </Router>
     );
